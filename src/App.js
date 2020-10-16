a76c4464c0293bfeebfd9be149ae89ffbf9bb37c
Kamal Thennakoon <thennakoon.kamal@gmail.com>
2020-10-17 00:53:03 +0530

    dynamic route system implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,99 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { AuthContext } from "./context/auth-context";
+import Login from "./pages/auth/login";
+import Logout from "./pages/auth/logout";
+import SignUp from "./pages/auth/signup";
+import MakePayment from "./pages/make-payment/make-payment";
 
-import MakePayment from './pages/make-payment/make-payment';
 
 export default function App() {
-	return <MakePayment />;
+  const [token, setToken] = useState();
+  const [name, setName] = useState();
+  const [mgr, setMgr] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const login = useCallback((name, token, mgr) => {
+    setToken(token);
+    setName(name);
+    setMgr(mgr);
+    
+
+    localStorage.setItem(
+      "mgrData",
+      JSON.stringify({
+        name: name,
+        token: token,
+        mgr: mgr,
+      })
+    );
+
+    console.log("auto logged in" + mgr + " " + token);
+    setIsLoading(false);
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setName(null);
+    setMgr(null);
+
+    localStorage.removeItem("mgrData");
+    localStorage.clear();
+    return <Redirect to={"/login"} />;
+  }, []);
+
+  // automatic login at start up (using local storage)
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("mgrData"));
+    if (storedData && storedData.token) {
+      login(storedData.name, storedData.token, storedData.mgr);
+    }
+    setIsLoading(false);
+  }, [login]);
+
+  let reactRoutes;
+  if (token) {
+    reactRoutes = (
+      <Switch>
+        <Route path="/" exact>
+          <MakePayment></MakePayment>
+        </Route>
+        <Route path="/logout" exact>
+          <Logout></Logout>
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    reactRoutes = (
+      <Switch>
+        <Route path="/login" exact>
+          <Login></Login>
+        </Route>
+        <Route path="/signup" exact>
+          <SignUp></SignUp>
+        </Route>
+        <Redirect to="/login" />
+      </Switch>
+    );
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        name: name,
+        mgr: mgr,
+        login: login,
+        logout: logout,
+      }}
+    >
+      {!isLoading&&<Router>{reactRoutes}</Router>}
+    </AuthContext.Provider>
+  );
 }
